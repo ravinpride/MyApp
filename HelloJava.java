ff682afdf3754e4d18ee18c47c8742a408ed45e7
Ubuntu <ravi.115014@gmail.com>
2020-12-17 16:37:48 +0000

    Pushing files to repository

---
+++ HelloJava.java
@@ -0,0 +1,5 @@
+class HelloJava{
+	public statis void main(String[] args){
+		System.out.println("Hello I am a Java Program. Thank you");
+}
+}
---
+++ index2.js
@@ -0,0 +1,3 @@
+var texts="I am your javaScript program";
+
+console.log(texts);
